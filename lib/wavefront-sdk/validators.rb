c2547eda374d5ba7c78bf127aa31965e5626d039
Robert Fisher <slackboy@gmail.com>
2017-03-26 00:29:48 +0000

    more validators, cleaner syntax

--- lib/wavefront-sdk/validators.rb
+++ lib/wavefront-sdk/validators.rb
@@ -1,62 +1,84 @@
+require_relative './exception'
+
 module Wavefront
   #
   # A module of mixins to validate input. The Wavefront documentation
   # lays down restrictions on types and sizes of various inputs, which
   # we will check on the user's behalf. Most of the information used in
   # this file comes from https://community.wavefront.com/docs/DOC-1031
-  # some comes from the Swagger API documentation.
+  # Some comes from the Swagger API documentation, some has come
+  # directly from Wavefront engineers.
   #
   module Validators
-    def valid_source?(source)
-      #
-      # Check a source, according to
-      #
-      unless source.is_a?(String) && source.match(/^[a-z0-9\-_\.]+$/) &&
-             source.length < 1024
-        raise Wavefront::Exception::InvalidSource
+    def wf_metric_name?(v)
+      if v.is_a?(String) && v.size < 1024 &&
+         (v.match(/^[\w\-\.]+$/) || v.match(%r{^\"[\w\-\.\/,]+\"$}))
+        return true
       end
-      true
+
+      raise Wavefront::Exception::InvalidMetricName
     end
 
-    def valid_string?(string)
+    def wf_string?(v)
       #
       # Only allows PCRE "word" characters, spaces, full-stops and
       # commas in tags and descriptions. This might be too restrictive,
       # but if it is, this is the only place we need to change it.
       #
-      string.match(/^[\-\w \.,]*$/)
+      return true if v.is_a?(String) && v.match(/^[\-\w \.,]*$/)
+
+      raise Wavefront::Exception::InvalidString
     end
 
-    def valid_path?(path)
-      raise Wavefront::Exception::InvalidMetricName unless \
-        path.is_a?(String) && path.match(/^[a-z0-9\-_\.]+$/) &&
-        path.length < 1024
-      true
+    def wf_source?(v)
+      return true if v.is_a?(String) && v.match(/^[\w\.\-]+$/) && v.size < 1024
+      raise Wavefront::Exception::InvalidSource
     end
 
-    def valid_value?(value)
-      raise Wavefront::Exception::InvalidMetricValue unless value.is_a?(Numeric)
-      true
+    def wf_value?(v)
+      return true if v.is_a?(Numeric)
+      raise Wavefront::Exception::InvalidMetricValue
     end
 
-    def valid_ts?(ts)
-      unless ts.is_a?(Time) || ts.is_a?(Date)
-        raise Wavefront::Exception::InvalidTimestamp
-      end
-      true
+    def wf_ts?(v)
+      return true if v.is_a?(Time) || v.is_a?(Date)
+      raise Wavefront::Exception::InvalidTimestamp
     end
 
-    def valid_tags?(tags)
+    def wf_point_tags?(tags)
       #
       # Operates on a hash of key-value point tags. These are
       # different from source tags.
       #
+      raise Wavefront::Exception::InvalidTag unless tags.is_a?(Hash)
+
       tags.each do |k, v|
-        unless (k.length + v.length < 254) && k.match(/^[a-z0-9\-_\.]+$/)
+        unless (k.size + v.size < 254) && k.match(/^[\w\-_\.]+$/)
           raise Wavefront::Exception::InvalidTag
         end
       end
       true
+    end
+
+    def wf_agent?(v)
+      if v.is_a?(String) && v.match(
+        /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/
+      )
+        return true
+      end
+
+      raise Wavefront::Exception::InvalidAgent
+    end
+
+    def wf_alert?(v)
+      #
+      # Alerts are identified by the epoch-nanosecond at which they
+      # were created.
+      #
+      v = v.to_s if v.is_a?(Numeric)
+
+      return true if v.is_a?(String) && v.match(/^\d{13}$/)
+      raise Wavefront::Exception::InvalidAlert
     end
   end
 end
