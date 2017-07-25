2ad3e282e45b4128d9e2f189bfce3fe11042c18d
Robert Fisher <slackboy@gmail.com>
2017-07-25 01:19:59 +0100

    search convenience method

--- spec/wavefront-sdk/search_spec.rb
+++ spec/wavefront-sdk/search_spec.rb
@@ -16,32 +16,32 @@ SEARCH_BODY = {
 #
 class WavefrontSearchTest < WavefrontTestBase
   def test_search
-    should_work(:search, ['agent', SEARCH_BODY], 'agent', :post,
+    should_work(:raw_search, ['agent', SEARCH_BODY], 'agent', :post,
                 JSON_POST_HEADERS, SEARCH_BODY.to_json)
-    should_work(:search, ['agent', SEARCH_BODY, true], 'agent/deleted',
+    should_work(:raw_search, ['agent', SEARCH_BODY, true], 'agent/deleted',
                 :post, JSON_POST_HEADERS, SEARCH_BODY.to_json)
-    assert_raises(ArgumentError) { wf.search }
-    assert_raises(ArgumentError) { wf.search('ALERT', 'junk') }
+    assert_raises(ArgumentError) { wf.raw_search }
+    assert_raises(ArgumentError) { wf.raw_search('ALERT', 'junk') }
   end
 
   def test_facet_search
-    should_work(:facet_search, ['agent', SEARCH_BODY],
+    should_work(:raw_facet_search, ['agent', SEARCH_BODY],
                 'agent/facets', :post, JSON_POST_HEADERS,
                 SEARCH_BODY.to_json)
 
-    should_work(:facet_search, ['agent', SEARCH_BODY, true],
+    should_work(:raw_facet_search, ['agent', SEARCH_BODY, true],
                 'agent/deleted/facets', :post, JSON_POST_HEADERS,
                 SEARCH_BODY.to_json)
 
-    should_work(:facet_search, ['agent', SEARCH_BODY, false, 'Tags'],
+    should_work(:raw_facet_search, ['agent', SEARCH_BODY, false, 'Tags'],
                 'agent/Tags', :post, JSON_POST_HEADERS,
                 SEARCH_BODY.to_json)
 
-    should_work(:facet_search, ['agent', SEARCH_BODY, true, 'Tags'],
+    should_work(:raw_facet_search, ['agent', SEARCH_BODY, true, 'Tags'],
                 'agent/deleted/Tags', :post, JSON_POST_HEADERS,
                 SEARCH_BODY.to_json)
 
-    assert_raises(ArgumentError) { wf.facet_search }
-    assert_raises(ArgumentError) { wf.facet_search('ALERT', 'junk') }
+    assert_raises(ArgumentError) { wf.raw_facet_search }
+    assert_raises(ArgumentError) { wf.raw_facet_search('ALERT', 'junk') }
   end
 end
