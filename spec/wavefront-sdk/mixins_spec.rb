e2e6cc3bf70a16077fb10068f70842f0a2e5e634
Robert Fisher <slackboy@gmail.com>
2018-04-03 13:59:44 +0100

    usual fixnum/integer conundrum

--- spec/wavefront-sdk/mixins_spec.rb
+++ spec/wavefront-sdk/mixins_spec.rb
@@ -9,17 +9,18 @@ require 'spy/integration'
 class WavefrontMixinsTest < MiniTest::Test
   include Wavefront::Mixins
 
+  # rubocop:disable Lint/UnifiedInteger
   def test_parse_time
     base_t = Time.now.to_i
     assert_equal parse_time(1_469_711_187), 1_469_711_187
     assert_equal parse_time('2016-07-28 14:25:36 +0100'), 1_469_712_336
-    assert_instance_of Integer, parse_time(Time.now)
-    assert_instance_of Integer, parse_time(Time.now, true)
+    assert_instance_of Fixnum, parse_time(Time.now)
+    assert_instance_of Fixnum, parse_time(Time.now, true)
     assert parse_time(Time.now) >= base_t
     assert parse_time(Time.now, true) >= base_t * 1000
     assert parse_time(Time.now, true) < base_t * 1001
-    assert_instance_of Integer, parse_time(Time.now)
-    assert_instance_of Integer, parse_time(Time.now, true)
+    assert_instance_of Fixnum, parse_time(Time.now)
+    assert_instance_of Fixnum, parse_time(Time.now, true)
     assert_raises(Wavefront::Exception::InvalidTimestamp) do
       parse_time('nonsense')
     end
@@ -31,6 +32,7 @@ class WavefrontMixinsTest < MiniTest::Test
 
     assert_equal trt_spy.calls.length, 2
   end
+  # rubocop:enable Lint/UnifiedInteger
 
   def test_relative_time
     base = Time.now
