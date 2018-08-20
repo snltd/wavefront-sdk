abedd22495396d31feb762e2b0f49fe43b73aafe
Robert Fisher <slackboy@gmail.com>
2018-08-20 17:17:20 +0100

    allow updating of filter regexes

--- lib/wavefront-sdk/externallink.rb
+++ lib/wavefront-sdk/externallink.rb
@@ -10,7 +10,8 @@ module Wavefront
     end
 
     def update_keys
-      %i[name template description]
+      %i[name template description metricFilterRegex sourceFilterRegex
+         pointTagFilterRegexes]
     end
 
     # GET /api/v2/extlink
